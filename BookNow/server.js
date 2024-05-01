2d0835f75bd4dba3f45930770ce6aad14ce57919
Chen Yue <yuechen049@gmail.com>
2024-05-01 15:59:07 -0400

    project setup

---
+++ BookNow/server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const courses = require('./routes/clients');
+const home = require('./routes/providers');
+const express = require('express');
+const app = express();
+
+// connect to database
+const connectionString = '';
+mongoose.connect(connectionString)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(() => console.error('Could not connect to MongoDB', err));
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+// TODO: add routes
+
+// host and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// listen on port and hostname
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
