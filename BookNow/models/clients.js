473018e0890a5856bfbcb14876c8e41704212690
Chen Yue <yuechen049@gmail.com>
2024-05-01 22:02:44 -0400

    added address and client models with validation and added routes for get, post, and put for clients

--- BookNow/models/clients.js
+++ BookNow/models/clients.js
@@ -1,27 +1,116 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-// create client model
-const Client = mongoose.model('client', new mongoose.Schema({
-    email: {
+// Address schema
+const addressSchema = new mongoose.Schema({
+    unitNumber: Number,
+    streetNumber: {
+        type: Number,
+        required: true
+    },
+    streetName: {
         type: String,
         required: true
     },
-    phone: {
+    city: {
         type: String,
         required: true
+    },
+    province: {
+        type: String,
+        required: true,
+        match: /^[A-Z]{2}$/,
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    postalCode: {
+        type: String,
+        required: true,
+        match: /^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$/,
     }
+});
+
+// Client schema
+const Client = mongoose.model('client', new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        lowercase: true,
+        trim: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        match: /^[0-9]{10}$/,
+        trim: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50,
+        trim: true
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'other']
+    },
+    dateOfBirth: {
+        type: Date
+    },
+    medicalConditions: {
+        type: String,
+        maxlength: 500,
+        trim: true
+    },
+    address: [addressSchema]
 }))
 
-// validate function
-function validateClient(client){
+// validate function for POST
+function validateClientAdd(client){
     const schema = Joi.object({
-        email: Joi.string().required(),
-        phone: Joi.string().required()
+        email: Joi.string().pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).required(),
+        phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        gender: Joi.string(),
+        dateOfBirth: Joi.date().less('now'),
+        medicalConditions: Joi.string(),
+        address: Joi.array()
+    });
+
+    return schema.validate(client);
+}
+
+// validate function for PUT
+function validateClientUpdate(client){
+    const schema = Joi.object({
+        email: Joi.string().pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+        phone: Joi.string().pattern(/^[0-9]{10}$/),
+        firstName: Joi.string(),
+        lastName: Joi.string(),
+        gender: Joi.string(),
+        dateOfBirth: Joi.date().less('now'),
+        medicalConditions: Joi.string(),
+        address: Joi.array()
     });
 
     return schema.validate(client);
 }
 
 exports.Client = Client;
-exports.validate = validateClient;
+exports.validateAdd = validateClientAdd;
+exports.validateUpdate = validateClientUpdate;
--- BookNow/routes/clients.js
+++ BookNow/routes/clients.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { Client, validate } = require('../models/clients');
+const { Client, validateAdd, validateUpdate } = require('../models/clients');
 const express = require('express');
 const router = express.Router();
 
@@ -10,16 +10,22 @@ router.get('/', async (req, res) => {
     res.send(clients);
 })
 
-// Post
+// POST
 router.post('/', async (req, res) => {
     // input validation
-    const { error } = validate(req.body);
+    const { error } = validateAdd(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     // create a new client
     const client = new Client({
         email: req.body.email,
-        phone: req.body.phone
+        phone: req.body.phone,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        gender: req.body.gender,
+        dateOfBirth: req.body.dateOfBirth,
+        medicalConditions: req.body.medicalConditions,
+        address: req.body.address
     });
 
     try {
@@ -29,6 +35,75 @@ router.post('/', async (req, res) => {
     } catch (ex) {
         console.log(ex.message);
         res.status(400).send(ex.message);
+    }
+});
+
+// PUT
+// update client info general
+router.put('/:id', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // find user to update
+        const client = await Client.findByIdAndUpdate(req.params.id,
+            {
+                $set: {
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    gender: req.body.gender,
+                    dateOfBirth: req.body.dateOfBirth,
+                    medicalConditions: req.body.medicalConditions,
+                    address: req.body.address
+                }
+            }
+        );
+
+        // if no course of this id
+        if (!client) return res.status(400).send("No client of this id exists.");
+
+        console.log(client);
+        res.send(client);
+    } catch (e) {
+        return res.status(400).send("No client of this id exists.");
+    }
+});
+
+// update client address
+router.put('/:id/addresses/:addrID', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // find user to update
+        const client = await Client.findByIdAndUpdate(req.params.id,
+            {
+                $set: {
+                    [`address.$[addr].unitNumber`]: req.body.address[0].unitNumber,
+                    [`address.$[addr].streetNumber`]: req.body.address[0].streetNumber,
+                    [`address.$[addr].streetName`]: req.body.address[0].streetName,
+                    [`address.$[addr].city`]: req.body.address[0].city,
+                    [`address.$[addr].province`]: req.body.address[0].province,
+                    [`address.$[addr].country`]: req.body.address[0].country,
+                    [`address.$[addr].postalCode`]: req.body.address[0].postalCode
+                }
+            },
+            {
+                'arrayFilters': [{"addr._id": req.params.addrID}]
+            }
+        );
+
+        // if no course of this id
+        if (!client) return res.status(400).send("No client of this id exists.");
+
+        console.log(client);
+        res.send(client);
+    } catch (e) {
+        return res.status(400).send("No client of this id exists.");
     }
 });
 
